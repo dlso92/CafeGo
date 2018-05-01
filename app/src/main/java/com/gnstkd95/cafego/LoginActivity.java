fc18c5f5114593f26d6b698f2ce266ffafca7fc0
hoon <ghkdrmaskfk@naver.com>
2018-05-01 09:30:09 +0900

    one

--- app/src/main/java/com/gnstkd95/cafego/LoginActivity.java
+++ app/src/main/java/com/gnstkd95/cafego/LoginActivity.java
@@ -1,30 +1,18 @@
 package com.gnstkd95.cafego;
 
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
 
 public class LoginActivity extends AppCompatActivity {
-    int RC_SIGN_IN = 9001;
     EditText ID;
     EditText PS;
     SignInButton google_Btn;
     Button gest_Btn;
-    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
